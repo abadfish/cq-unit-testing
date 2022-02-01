d952a1d4a5adb18c37529957619c165f624e3372
abadfish <kirsten.ofarrell@gmail.com>
2022-01-31 21:16:30 -0500

    stubbed a product for priceAfterDiscount test

--- src/tests/calculatePriceAfterDiscount.spec.ts
+++ src/tests/calculatePriceAfterDiscount.spec.ts
@@ -1,10 +1,26 @@
 import calculatePriceAfterDiscount from "../priceCalculator/calculatePriceAfterDiscount";
+import { Brand, Product, ProductType } from "../types";
+
+const testBrand: Brand = {
+  id: "test-brand",
+  name: "test-brand-name",
+};
+
 
 describe("Calculate price after discount", () => {
 	it("Returns a price with the discount applied", () => {
-		const price = 50
-		const discount = 50
-		const value = calculatePriceAfterDiscount(price, discount)
-		expect(value).toEqual(25)
+		const prodWithDiscount: Product = {
+      name: "Nike Sport Shoes",
+      id: "test-product-with-discount",
+      net_price: 16,
+      discount: {
+        isEnabled: true,
+        percentage: 12,
+      },
+      brand: testBrand,
+      type: ProductType.FOOD,
+    };
+		const value = calculatePriceAfterDiscount(prodWithDiscount.net_price, prodWithDiscount.discount.percentage)
+		expect(value).toEqual(16 * (1 - .12) )
 	})
 })
