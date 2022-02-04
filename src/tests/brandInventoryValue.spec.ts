2d5d4aa596dfea86d2871f4ef74f05d97511c2b8
abadfish <kirsten.ofarrell@gmail.com>
2022-02-04 14:34:12 -0500

    updated brandInventoryValue.spec to use given, when, then

--- src/tests/brandInventoryValue.spec.ts
+++ src/tests/brandInventoryValue.spec.ts
@@ -9,7 +9,7 @@ const testBrand: Brand = {
 };
 describe("Brand inventory value", () => {
   describe("when the inventory contains one brand item", () => {
-    it("returns the price of the item", () => {
+    it("then returns the price of the item", () => {
       // ACT
       const value = brandInventoryValue([chocolatePremium], foodBrandPremium);
       // ASSERT
@@ -17,13 +17,13 @@ describe("Brand inventory value", () => {
     });
   });
 	describe("when inventory contains 0 items", () => {
-		it("returns zero", () => {
+		it("then returns zero", () => {
 			const value = brandInventoryValue([], testBrand);
 			expect(value).toEqual(0);
 		})
 	})
 	describe("when a brand is chosen", () => {
-    it("returns zero if there are no products with a brand id that matches the chosen brand", () => {
+    it("then returns zero if there are no products with a brand id that matches the chosen brand", () => {
       const value = brandInventoryValue([muffin, bagelsDiscounted], testBrand);
       expect(value).toEqual(0);
     });
